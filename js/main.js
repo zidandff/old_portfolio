c555a488d577fbe9ceab82230e24d087e6e1e5fb
Zidan M. Daffa <zidandaff@gmail.com>
2020-11-07 21:49:12 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const toggleMenu = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".nav-links")
+const navLinks = document.querySelectorAll(".nav-links li")
+const categories = document.querySelectorAll('.category-item')
+const projects = document.querySelectorAll(".card-project")
+const toTop = document.querySelector(".back-to-top")
+
+// navbar
+toggleMenu.addEventListener('click', function(){
+    navbar.classList.toggle("slide")
+})
+navLinks.forEach((e)=>{
+    e.addEventListener('click', ()=>{
+        navbar.classList.remove("slide")
+    })
+})
+
+window.addEventListener('scroll', ()=> {
+    navbar.classList.remove("slide")
+})
+
+// smooth scroll
+new SmoothScroll('.nav-links a[href*="#"]', {
+    speed: 1000,
+    easing: 'easeInOutQuad'
+});
+
+new SmoothScroll('.back-to-top a[href*="#"]', {
+    speed: 1000,
+    easing: 'easeInOutQuad'
+});
+
+// filter project
+categories.forEach(category => {
+    category.addEventListener('click', ()=> {
+        let filterSelected = category.getAttribute("data-target")
+        
+        // add/remove active class
+        categories.forEach(e => e.classList.remove("active"))
+        category.classList.add("active")
+
+        // hide & show 
+        projects.forEach(project => {
+            project.classList.add("fade-up")
+            if( filterSelected == "all" ){
+                project.style.display = "block"
+            }
+            else if( !project.classList.contains(filterSelected) ){
+                project.style.display = "none"
+            }
+            else {
+                project.style.display = "block"
+            }
+            // hapus class fade
+            setTimeout(() => {
+                project.classList.remove('fade-up')
+            }, 500);
+        })
+    })
+})
+
+// back to top
+
+
+window.addEventListener("scroll", ()=> {
+    if( window.scrollY > document.querySelector(".service").offsetTop ){
+        toTop.style.display = "flex"
+    }
+    else {
+        toTop.style.display = "none"
+    }
+})
---
+++ js/parallax.js
@@ -0,0 +1,4 @@
+new Parallax(document.getElementById('triangle-blur'));
+new Parallax(document.getElementById('box-blur'));
+new Parallax(document.getElementById('triangle'));
+
